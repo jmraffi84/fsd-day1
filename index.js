8d2273f93111512c9b2958eac592285a084d09bf
jmraffi84 <jmraffi@gmail.com>
2023-11-03 17:06:31 +0530

    response type json added

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const http = require('http')
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World')
+
+let notes = [
+    {
+        id: 1,
+        content: 'HTML is easy',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'Backend application using NodeJs',
+        important: true
+    },
+    {
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        important: true
+    },
+    {
+        id: 4,
+        content: 'Browser can execute only Javascript',
+        important: false
+    },
+]
+
+const app = http.createServer((req, res) => {
+    // res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(JSON.stringify(notes))
 })
 
 const PORT = 3001;
